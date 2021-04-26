d0a6cb2d65a979db51facea8becb8cf021077966
Jun Park <jhpp114node@gmail.com>
2021-04-26 09:24:57 -0700

    testing for merge conflict

--- src/com/c2j/musicjukebox/music/client/MusicJukeboxClient.java
+++ src/com/c2j/musicjukebox/music/client/MusicJukeboxClient.java
@@ -3,5 +3,10 @@ package com.c2j.musicjukebox.music.client;
 public class MusicJukeboxClient {
     public static void main(String[] args) {
         System.out.println("client");
+        System.out.println("asdjfklasdf");
+        int i = 0;
+        System.out.println("asdjfklasdf");
+        int asdfasdf = 12313;
+        System.out.println(asdfasdf);
     }
 }
