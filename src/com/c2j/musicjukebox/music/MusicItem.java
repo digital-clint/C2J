799567871091a0038101149d7e12962dcafb213f
Jun Park <jhpp114node@gmail.com>
2021-04-26 12:27:45 -0700

    added basic template of the MusicItem class

--- src/com/c2j/musicjukebox/music/MusicItem.java
+++ src/com/c2j/musicjukebox/music/MusicItem.java
@@ -1,11 +1,13 @@
 package com.c2j.musicjukebox.music;
 
 import java.sql.Date;
+import java.util.Objects;
 
-public class MusicItem {
+public class MusicItem implements Comparable<MusicItem> {
     private String title;
     private String artist;
-    // place holder data for enum
+    private MusicGenre musicGenre;
+    private MusicRegions musicRegions;
     private Date year;
     private String album;
     private static final double DEFAULT_PRICE = 0.25;
@@ -14,15 +16,18 @@ public class MusicItem {
 
     }
 
-    // both of two enum must go in here
-    public MusicItem(String title, String artist, String year) {
-        this(title, artist,Date.valueOf(year));
+    // Method overloading (String for year)
+    public MusicItem(String title, String artist, MusicGenre musicGenre, MusicRegions musicRegions ,String year, String album) {
+        this(title, artist, musicGenre, musicRegions ,Date.valueOf(year), album);
     }
 
-    public MusicItem(String title, String artist, Date year) {
+    public MusicItem(String title, String artist, MusicGenre musicGenre, MusicRegions musicRegions, Date year, String album) {
         setTitle(title);
         setArtist(artist);
+        setMusicGenre(musicGenre);
+        setMusicRegions(musicRegions);
         setYear(year);
+        setAlbum(album);
     }
 
     public String getTitle() {
@@ -45,6 +50,26 @@ public class MusicItem {
         return year;
     }
 
+    // return as MusicGenre (Enum)
+    public MusicGenre getMusicGenre() {
+        return musicGenre;
+    }
+
+    // @Param Enum musicGenre
+    public void setMusicGenre(MusicGenre musicGenre) {
+        this.musicGenre = musicGenre;
+    }
+
+    // return as MusicRegion (Enum)
+    public MusicRegions getMusicRegions() {
+        return musicRegions;
+    }
+
+    // @Param Enum MusicRegion
+    public void setMusicRegions(MusicRegions musicRegions) {
+        this.musicRegions = musicRegions;
+    }
+
     public void setYear(Date year) {
         this.year = year;
     }
@@ -57,14 +82,38 @@ public class MusicItem {
         this.album = album;
     }
 
-    // toString method must be updated after we have enum
+    // ==========================================
+    // =========== Override methods =============
     @Override
     public String toString() {
         return "MusicItem{" +
                 "title='" + title + '\'' +
                 ", artist='" + artist + '\'' +
+                ", musicGenre=" + musicGenre +
+                ", musicRegions=" + musicRegions +
                 ", year=" + year +
                 ", album='" + album + '\'' +
                 '}';
+    }
+
+    @Override
+    public int compareTo(MusicItem o) {
+        // basic compareTo or sorting is use the title
+        return this.getTitle().compareTo(o.getTitle());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MusicItem musicItem = (MusicItem) o;
+        return Objects.equals(title, musicItem.title) && Objects.equals(artist, musicItem.artist)
+                && musicGenre == musicItem.musicGenre && musicRegions == musicItem.musicRegions
+                && Objects.equals(year, musicItem.year) && Objects.equals(album, musicItem.album);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, artist, musicGenre, musicRegions, year, album);
     }
 }
