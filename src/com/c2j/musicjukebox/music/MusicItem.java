e9496ac9edad41af3f3942a2870d8bb7bb78f9c8
Jun Park <jhpp114node@gmail.com>
2021-04-26 10:27:09 -0700

    added music utilites interface and the MusicItem class

---
+++ src/com/c2j/musicjukebox/music/MusicItem.java
@@ -0,0 +1,60 @@
+package com.c2j.musicjukebox.music;
+
+import java.sql.Date;
+
+public class MusicItem {
+    private String title;
+    private String artist;
+    // place holder data for enum
+    private Date year;
+    private static final double DEFAULT_PRICE = 0.25;
+
+    public MusicItem() {
+
+    }
+
+    // both of two enum must go in here
+    public MusicItem(String title, String artist, String year) {
+        this(title, artist,Date.valueOf(year));
+    }
+
+    public MusicItem(String title, String artist, Date year) {
+        setTitle(title);
+        setArtist(artist);
+        setYear(year);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    // toString method must be updated after we have enum
+    @Override
+    public String toString() {
+        return "MusicItem{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
--- src/com/c2j/musicjukebox/music/MusicUtilities.java
+++ src/com/c2j/musicjukebox/music/MusicUtilities.java
@@ -1,4 +1,14 @@
 package com.c2j.musicjukebox.music;
 
+import java.util.Collection;
+
 public interface MusicUtilities {
+    public Collection<MusicItem> findByTitle();
+    public Collection<MusicItem> findByRegion();
+    public Collection<MusicItem> findByArtist();
+    public Collection<MusicItem> findByGenre();
+    public Collection<MusicItem> findByYear();
+    public void displayAllMusic();
+    public void play();
+    public void printInvoice();
 }
