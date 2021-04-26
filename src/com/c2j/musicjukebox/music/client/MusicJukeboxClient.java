fc62401449f9629d8ef941eb7eb5af26c96b569a
Robertson <clintrb@amazon.com>
2021-04-26 09:26:50 -0700

    Testing merge conflict

--- src/com/c2j/musicjukebox/music/client/MusicJukeboxClient.java
+++ src/com/c2j/musicjukebox/music/client/MusicJukeboxClient.java
@@ -3,5 +3,10 @@ package com.c2j.musicjukebox.music.client;
 public class MusicJukeboxClient {
     public static void main(String[] args) {
         System.out.println("client");
+        System.out.println("I'm testing a merge conflict");
+        System.out.println("I'm testing a merge conflict");
+        System.out.println("I'm testing a merge conflict");
+        System.out.println("I'm testing a merge conflict");
+        System.out.println("I'm testing a merge conflict");
     }
 }
