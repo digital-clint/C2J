7a11146aa50f7d23bd5d6fa9c905f791c72694c7
Hurtault <jhurtaul@amazon.com>
2021-04-26 12:59:02 -0700

    Added JukeUserInputUtiliesImpl class

--- src/com/c2j/musicjukebox/user/JukeboxUserInputUtilities.java
+++ src/com/c2j/musicjukebox/user/JukeboxUserInputUtilities.java
@@ -1,4 +1,5 @@
 package com.c2j.musicjukebox.user;
 
 public interface JukeboxUserInputUtilities {
+
 }
---
+++ src/com/c2j/musicjukebox/user/JukeboxUserInputUtilitiesImpl.java
@@ -0,0 +1,12 @@
+package com.c2j.musicjukebox.user;
+
+public class JukeboxUserInputUtilitiesImpl implements JukeboxUserInputUtilities {
+
+    private String availableMusicTitle;
+    private String availableMusicRegion;
+    private String availableMusicArtist;
+    private String availableMusicAlbum;
+    private String getAvailableMusicYear;
+    private String totalJukeBoxAmount;
+
+}
