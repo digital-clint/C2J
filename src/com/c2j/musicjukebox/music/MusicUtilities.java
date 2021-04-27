f818705268c793242dac98a59bf9696a02299bf0
Jun Park <jhpp114node@gmail.com>
2021-04-27 08:17:56 -0700

    added parameter for MusicUtil Interface

--- src/com/c2j/musicjukebox/music/MusicUtilities.java
+++ src/com/c2j/musicjukebox/music/MusicUtilities.java
@@ -1,14 +1,24 @@
 package com.c2j.musicjukebox.music;
 
+import java.sql.Date;
 import java.util.Collection;
 
 public interface MusicUtilities {
-    public Collection<MusicItem> findByTitle();
-    public Collection<MusicItem> findByRegion();
-    public Collection<MusicItem> findByArtist();
-    public Collection<MusicItem> findByGenre();
-    public Collection<MusicItem> findByYear();
-    public Collection<MusicItem> findByAlbum();
+    public Collection<MusicItem> findByTitle(String targetTitle);
+    // method overloading param is String
+    public Collection<MusicItem> findByRegion(String targetByRegion);
+    // method overloading param is Enum
+    public Collection<MusicItem> findByRegion(MusicRegions targetByRegion);
+    public Collection<MusicItem> findByArtist(String targetArtist);
+    // method overloading param is String
+    public Collection<MusicItem> findByGenre(String targetGenre);
+    // method overloading param is Enum
+    public Collection<MusicItem> findByGenre(MusicGenre targetGenre);
+    // method overloading param is String
+    public Collection<MusicItem> findByYear(String targetYear);
+    // method overloading param is Date
+    public Collection<MusicItem> findByYear(Date targetYear);
+    public Collection<MusicItem> findByAlbum(String targetAlbum);
     public void displayAllMusic();
     public void play();
     public void printInvoice();
