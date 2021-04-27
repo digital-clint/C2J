ee8dac9217f817ab57ecc9d5c79a07342602f2fa
Robertson <clintrb@amazon.com>
2021-04-27 09:50:28 -0700

    Added break statements for switch cases and input validation for jukebox validation

--- src/com/c2j/musicjukebox/menu/MusicJukeBoxMenu.java
+++ src/com/c2j/musicjukebox/menu/MusicJukeBoxMenu.java
@@ -32,68 +32,84 @@ public class MusicJukeBoxMenu {
         System.out.println();
         System.out.print("Please enter the number of your selection: ");
         userInput = scan.nextLine();
-        userNumChoice = Integer.parseInt(userInput);
+        System.out.println("-------------------------------------------");
+
+        try {
+            userNumChoice = Integer.parseInt(userInput);
+            if(userNumChoice != (int)userNumChoice){
+                throw new NumberFormatException();
+            }
+        }
+        catch(NumberFormatException e) {
+            System.out.println("Please enter a valid number!");
+            System.out.println();
+            displayJukeboxOptions();
+        }
 
         switch (userNumChoice){
             case 1:
                 viewAllSongs();
+                break;
 
             case 2:
                 pickSongFromGenre();
+                break;
 
             case 3:
                 pickSongFromRegion();
+                break;
 
             case 4:
                 findSongByTitle();
+                break;
 
             case 5:
                 findSongByReleaseYear();
+                break;
 
             case 6:
                 findSongByAlbumName();
+                break;
 
             case 7:
                 loginAsAdmin();
+                break;
 
             default:
-                System.out.println("Invalid Selection");
-
+                System.out.println("Invalid Selection..Please try again");
+                System.out.println();
+                displayJukeboxOptions();
         }
     }
 
     //Created shell for methods below.
     //Will Add logic later
     public void viewAllSongs(){
-
+        System.out.println("case 1");
     }
 
     public void pickSongFromGenre(){
-
+        System.out.println("case 2");
     }
 
     public void pickSongFromRegion(){
-
+        System.out.println("case 3");
     }
 
     public void findSongByTitle(){
-
+        System.out.println("case 4");
     }
 
     public void findSongByReleaseYear(){
-
+        System.out.println("case 5");
     }
 
     public void findSongByAlbumName(){
-
+        System.out.println("case 6");
     }
 
     public void loginAsAdmin(){
-
+        System.out.println("case 7");
     }
-
-
-
-
 
 }
