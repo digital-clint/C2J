6ccc6db829bd92ff33bec7bd764ce88a05e11187
Hurtault <jhurtaul@amazon.com>
2021-04-28 21:38:18 -0700

    Added validation to songRequested class

---
+++ src/com/c2j/musicjukebox/menu/menustuff.java
@@ -0,0 +1,9 @@
+package com.c2j.musicjukebox.menu;
+
+public class menustuff {
+    public static void main(String[] args) {
+        MusicJukeBoxMenu menu = new MusicJukeBoxMenu();
+        menu.start();
+    }
+
+}
