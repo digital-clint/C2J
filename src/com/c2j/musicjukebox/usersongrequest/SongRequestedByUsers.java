84785a95eac5e2fb744d66b3843b2c246fd8c262
Hurtault <jhurtaul@amazon.com>
2021-04-28 19:56:45 -0700

    Deleted main method from SongRequested by users

--- src/com/c2j/musicjukebox/usersongrequest/SongRequestedByUsers.java
+++ src/com/c2j/musicjukebox/usersongrequest/SongRequestedByUsers.java
@@ -5,9 +5,11 @@ import com.c2j.musicjukebox.menu.MusicJukeBoxMenu;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+
+// displays to admin song that has already been requested by user
 public class SongRequestedByUsers extends MusicJukeBoxMenu {
-    public static void main(String[] args) {
-        try{
+            public void musicRequested(String text){
+            try{
             BufferedReader songsAlreadyRequested = new BufferedReader(
                     new FileReader("C:\\Users\\jhurtaul\\Desktop\\SDE Cohort\\StudentWork\\Java_LabSetup_Part-2\\StudentWork\\IntmJ\\workspace\\C2J"));
                 String songFile;
